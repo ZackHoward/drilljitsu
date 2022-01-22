a8488d9b1345a0aa137c01d2195235babc68eb62
Zack Howard <zhoward1994@protonmail.com>
2022-01-21 23:03:22 -0500

    trying to fix the delete function so it removes from listbox and list, but it still isnt working

--- main_tkinter.py
+++ main_tkinter.py
@@ -36,21 +36,15 @@ output_submission = ttk.Label(tab1, textvariable = submission, font = 8, borderw
 
 selection_label = ttk.Label(tab1, text = 'Select body part or difficulty').grid(column = 2, row = 1, sticky = W)
 
-drill_button = ttk.Button(tab1, text = 'DRILL', width = 20, command = lambda:[(practice_submission_body(), practice_submission_difficulty())]).grid(column = 2, row = 4, sticky = W)
+drill_button = ttk.Button(tab1, text = 'DRILL', width = 15, command = lambda:[(practice_submission_body(), practice_submission_difficulty())]).grid(column = 2, row = 4, sticky = W)
 
-search_youtube_button = ttk.Button(tab1, text = 'LEARN', width = 20, command = lambda: search_youtube()).grid(column = 2, row = 5, sticky = W)
+search_youtube_button = ttk.Button(tab1, text = 'LEARN', width = 15, command = lambda: search_youtube()).grid(column = 2, row = 5, sticky = W)
 
-youtube_label = ttk.Label(tab1, text = 'Click LEARN!').grid(column = 3, row = 5, sticky = (W, E))
-
-for child in tab1.winfo_children(): 
-    child.grid_configure(padx = 5, pady = 5)
-    
-for child in tab2.winfo_children(): 
-    child.grid_configure(padx = 5, pady = 5)
+youtube_label = ttk.Label(tab1, text = 'Click LEARN for YouTube!').grid(column = 3, row = 5, sticky = (W, E))
 
 drop_menu.focus()
 
-# Lists of submissions based on body part and difficulty
+# Lists of submissions based on body part
 arm = [
     'Straight Armbar', 'Americana Armbar', 'Kimura Armlock', 'Biceps Slicer', 'Omoplata', 'Wristlock', 'Inverse Armbar', 'Marceloplata', 'Baratoplata', 'Monoplata', 'Mir Lock', 'Hammerlock' 
 ]
@@ -63,11 +57,34 @@ neck = [
     'Rear Naked Choke', 'Triangle Choke', 'Bow and Arrow Choke', 'Ezekiel Choke', 'Guillotine', 'Cross Collar Choke', 'Short Choke', 'Baseball Choke', 'D\'Arce Choke', 'Head and Arm Choke', 'North-South Choke', 'Crucifix Choke', 'Thrust Choke', 'Anaconda Choke', 'Neck Crank', 'Twister', 'Peruvian Necktie', 'Loop Choke', 'Papercutter Choke', 'Gogoplata', 'Step Over Choke', 'Von Flue Choke', 'Lapel Half Nelson'
 ]
 
+all = arm + leg + neck
+
+# Display all submissions in settings tab for removal
 list_of_submissions = Listbox(tab2, height = 7, width = 25, bg = 'white', activestyle = 'dotbox')
 list_of_submissions.grid(column = 3, row = 1, sticky = W)
-for c in arm + leg + neck:
+for c in all:
     list_of_submissions.insert(END, c)
+    
+remove_submission_button = ttk.Button(tab2, text = 'REMOVE', width = 15, command = lambda: delete()).grid(column = 2, row = 1, sticky = W)
 
+def delete():
+    selection = list_of_submissions.curselection()
+    list_of_submissions.delete(selection)
+    all.insert(0, all.pop(all.index(selection[0])))
+    all.pop(selection[0])
+        
+#def delete_list():
+    #for i in list_of_submissions.curselection():
+        #list_removal = list_of_submissions.get(i)
+        #if list_removal in arm:
+            #arm.remove(list_removal)
+        #elif list_removal in leg:
+            #leg.remove(list_removal)
+        #elif list_removal in neck:
+            #neck.remove(list_removal)
+        #else: None
+                
+# Lists of submissions based on difficulty
 hard = ['Biceps Slicer', 'Marceloplata', 'Baratoplata', 'Monoplata', 'Hammerlock', 'Knee Slicer', 'Reverse Heel Hook', 'Banana Split Hip Lock', 'Crucifix Choke', 'Anaconda Choke', 'Peruvian Necktie', 'Gogoplata', 'Step Over Choke', 'Von Flue Choke', 'Lapel Half Nelson'
     
 ]
@@ -107,8 +124,8 @@ def practice_submission_difficulty():
         return
     
 # Function for adding submissions into lists    
-def add_submission():
-    add_which = input('Would you like to add an arm, leg, or neck submission?: ')
+#def add_submission():
+    #add_which = 
     if add_which == 'arm':
         return arm.extend(input('What arm submission would you like to add?: '))
     elif add_which == 'leg':
@@ -117,24 +134,20 @@ def add_submission():
         return neck.extend(input('Which neck submission would you like to add?: '))
     else:
         return 'Please choose a valid submission to add: arm, leg or neck.'
-
-# Function for removing submissions from lists
-def remove_submission():
-    remove_which = input('Which submission would you like to remove?: ')
-    if remove_which in arm:
-        return arm.remove(remove_which)
-    elif remove_which in leg:
-        return leg.remove(remove_which)
-    elif remove_which in neck:
-        return neck.remove(remove_which)
-    else:
-        return 'The submission you entered is not in the application.'
-    
+ 
+# Function for searching youtube, replacing whitespace with '+'    
 def search_youtube():
     def urlify(s):
         s = re.sub(r"\s+", '+', s) 
         return s   
     no_space_submission = urlify(submission.get())
     webbrowser.open('https://www.youtube.com/results?search_query=' + no_space_submission)
+        
+# makes windows in app look better, don't understand exactly how
+for child in tab1.winfo_children(): 
+    child.grid_configure(padx = 5, pady = 5)
+    
+for child in tab2.winfo_children(): 
+    child.grid_configure(padx = 5, pady = 5)
 
 root.mainloop()
